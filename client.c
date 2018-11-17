c346486a9b663f40d0a8fa58269887dfc0135c58
winnie huang <iamwinnie1014@gmail.com>
2018-11-17 15:58:30 +0800

    First try

--- client.c
+++ client.c
@@ -1,8 +1,90 @@
-#include "client.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
+char request[200];
 
 int main(int argc, char *argv[])
 {
-	return 0;
-}
+    /*
+    for(int i=0;i < argc;i++){
 
+    }
+    */
+
+    //request = NULL;
+    char *file_or_dir;
+    char *local_input;
+    char *port_input;
+    //char request;
+
+    file_or_dir = argv[2];
+    local_input = argv[4];
+    port_input = argv[6];
+    memset(request,0,sizeof(request));
+
+    printf("%s\n",file_or_dir);
+    sprintf(request, "GET %s HTTP/1.x\r\nHOST: %s:%s\r\n",file_or_dir,local_input,port_input);
+    //request = strcat(request,"GET ");
+    //request = strcat(request,file_or_dir);
+    printf("%s\n",request);
+    /*
+    //request = strcat(request ," ");
+    request = strcat(request," HTTP/1.x\r\nHOST: ");
+    request = strcat(request,local_input);
+    request = strcat(request,":");
+    request = strcat(request,port_input);
+    request = strcat(request,"\r\n");
+    printf("%s\n",request);
+    //check file_or_dir start with /
+    //false: status 400
+    */
+
+
+
+
+
+    //set up socket
+    int sockfd = 0;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1) {
+        printf("Fail to create a socket.");
+    }
+
+    //socket connect
+
+    struct sockaddr_in info;
+    bzero(&info,sizeof(info));
+    info.sin_family = AF_INET;
+
+    //localhost test
+    info.sin_addr.s_addr = inet_addr("127.0.0.1");
+    info.sin_port = htons(1234);
+
+
+    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
+    if(err==-1) {
+        printf("Connection error");
+    }
+
+
+    //Send a message to server
+    //strcpy(request,)
+    //char message[] = {"Hi there"};
+    char receiveMessage[100] = {};
+    send(sockfd,request,sizeof(request),0);
+    recv(sockfd,receiveMessage,sizeof(receiveMessage),0);
+
+    printf("%s",receiveMessage);
+    printf("close Socket\n");
+    memset(request,0,sizeof(request));
+    close(sockfd);
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,11 +1,105 @@
-#include "server.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 
+char msg[10000];
+char get_rqst_data;
 
 int main(int argc, char *argv[])
+
 {
 
+    //usage : ./server -r root -p port -n thread number
 
+
+    //??
+    //int thread_number = (int)(argv[6]);
+
+    char *root;
+    root = argv[2];
+
+
+    memset(msg,0,sizeof(msg));
+
+
+    //set up socket
+    char inputBuffer[256] = {};
+    char message[] = {"Hi,this is server.\n"};
+    int sockfd = 0,forClientSockfd = 0;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1) {
+        printf("Fail to create a socket.");
+    }
+
+    //socket connection
+    struct sockaddr_in serverInfo,clientInfo;
+    int addrlen = sizeof(clientInfo);
+    bzero(&serverInfo,sizeof(serverInfo));
+
+    serverInfo.sin_family = AF_INET;
+    serverInfo.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverInfo.sin_port = htons(1234);
+    bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
+    listen(sockfd,5);
+
+    while(1) {
+        forClientSockfd = accept(sockfd,(struct sockaddr*) &clientInfo, &addrlen);
+        recv(forClientSockfd,inputBuffer,sizeof(inputBuffer),0);
+        printf("Get:%s\n",inputBuffer);
+        //get request
+        char* delim = " ";
+        char* pch = NULL;
+        pch = strtok(inputBuffer,delim);
+        printf("pch:%s\n",pch);
+        //int i=0;
+        int c = 0;
+        char *file_or_dir;
+        char *host_input;
+        /*
+        while(pch!=NULL){
+            printf("%s\n",pch);
+            pch = strtok(NULL,delim);
+            //i++;
+        }
+        */
+
+
+        for(int i=0; i<4; i++) {
+            //printf("%d : %s\n",i,pch);
+            if(i == 1) {
+                file_or_dir = pch;
+            }
+            if(i == 3) {
+                host_input = pch;
+            }
+            pch = strtok(NULL,delim);
+        }
+        //printf("dir : %s\n",file_or_dir);
+
+        //printf("Get:%s\n",inputBuffer);
+        //check path or dir
+
+
+        send(forClientSockfd,message,sizeof(message),0);
+        //printf("Get:%s\n",inputBuffer);
+    }
+    return 0;
 }
 
+/*
+void* thread(void* args){
+    //do
 
+    //leave
+    pthread_exit(NULL);
+}
+*/
